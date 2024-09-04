b54ff017d52790a1480bc37da66e474ee8accbc7
nikhils <shakyanikhil2003@gmail.com>
2024-09-04 21:44:02 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Clock from "./assets/clock";
+function App() {
+  return (
+    <div>
+      <Clock />
+    </div>
+  );
+}
+export default App;
---
+++ src/assets/Clock.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+  function formatTime() {
+    let h = time.getHours();
+    const m = time.getMinutes();
+    const s = time.getSeconds();
+    const meridiem = h >= 12 ? "PM" : "AM";
+    h = h % 12 || 12;
+    return `${h}:${m}:${s} ${meridiem}`;
+  }
+  return (
+    <div className="containeer">
+      <div className="clock">
+        <span>{formatTime()}</span>
+      </div>
+    </div>
+  );
+}
+export default Clock;
